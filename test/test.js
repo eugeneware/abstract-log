66f788aad161acbb631508a62a44dcca1f97fb4b
Eugene Ware <eugene@noblesamurai.com>
2017-04-17 23:04:00 +1000

    use stream-to-promise

--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 const tape = require('tape');
 const it = require('tape-promise').default(tape);
 const abstractLog = require('..');
+const streamToPromise = require('stream-to-promise');
 
 it('should be able to append to the log', async (t) => {
   t.plan(1);
@@ -27,32 +28,10 @@ it('should be able to stream from a log offset', async (t) => {
     await log.append({ msg: `hello world ${i}` });
   }
 
-  let results = [];
-  await streamToPromise(
-    log.createReadStream()
-      .on('data', (data) => {
-        t.assert(data.msg.match(/hello world [0-9]/gim));
-        results.push(data);
-      })
-      .once('end', () => {
-        t.equal(results.length, 5);
-      })
-   );
+  let results = await streamToPromise(log.createReadStream());
+  results.forEach((data, i) => {
+    t.equal(data.msg, `hello world ${i}`);
+  });
+  t.equal(results.length, 5);
   t.end();
 });
-
-function streamToPromise (rs) {
-  return new Promise((resolve, reject) => {
-    rs
-      .once('error', (err) => {
-        setImmediate(() => {
-          reject(err);
-        });
-      })
-      .once('end', () => {
-        setImmediate(() => {
-          resolve();
-        });
-      });
-  });
-}
